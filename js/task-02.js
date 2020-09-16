6d0822246392135d05c70101a560c35990a8123a
Evgeniya3010 <susskaya86@gmail.com>
2020-09-16 03:43:54 +0300

    выполняет второе задание

---
+++ js/task-02.js
@@ -0,0 +1,9 @@
+const invoice = 100;
+const stock = 100;
+
+const message =
+  invoice <= stock
+    ? "Заказ оформлен, с вами свяжется менеджер"
+    : "На складе недостаточно товаров!";
+
+console.log(message);
