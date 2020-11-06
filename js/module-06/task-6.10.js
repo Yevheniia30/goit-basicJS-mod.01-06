07fb2cd39f30eeb115a14fb1ff9fc72465d91ff4
Evgeniya3010 <susskaya86@gmail.com>
2020-11-06 03:08:48 +0200

    задание 6.9

---
+++ js/module-06/task-6.10.js
@@ -0,0 +1,14 @@
+// Write code under this line
+const getSortedUniqueSkills = (array) => [...array];
+
+// console.log(getSortedUniqueSkills(users));
+/* [ 'adipisicing', 'amet',
+ 'anim', 'commodo',
+ 'culpa', 'elit',
+ 'ex', 'ipsum',
+ 'irure', 'laborum',
+ 'lorem', 'mollit',
+ 'non', 'nostrud',
+ 'nulla', 'proident',
+ 'tempor', 'velit',
+ 'veniam' ]; */
