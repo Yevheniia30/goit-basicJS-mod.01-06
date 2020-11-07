5fe86114c9b43310442c1526cbe2ba6e5f4d6c5b
Evgeniya3010 <susskaya86@gmail.com>
2020-11-07 22:57:41 +0200

    задание 6.10

--- js/module-06/task-6.10.js
+++ js/module-06/task-6.10.js
@@ -1,7 +1,22 @@
-// Write code under this line
-const getSortedUniqueSkills = (array) => [...array];
+// reduce, filter, sort
+// Получи массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
-// console.log(getSortedUniqueSkills(users));
+// Слияние массивов:
+
+// const odd = [1, 3, 5];
+// const even = [2, 4, 6];
+
+// // 1
+// [...odd, ...even];
+// //  [1, 3, 5, 2, 4, 6]
+
+// // 2
+// odd.concat(even)
+// //  [1, 3, 5, 2, 4, 6]
+// Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
+const getSortedUniqueSkills = array => array.reduce((acc, {skills}) => [...acc, ...skills], []).filter((skill, index, array) => array.indexOf(skill) === index).sort();                      ;
+
+console.log(getSortedUniqueSkills(users));
 /* [ 'adipisicing', 'amet',
  'anim', 'commodo',
  'culpa', 'elit',
